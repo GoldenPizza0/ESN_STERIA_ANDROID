f61611651b5c0132ac62152b883106dfcf842747
GoldenPizza0 <126585783+GoldenPizza0@users.noreply.github.com>
2025-05-23 15:29:17 +0200

    Update RetrofitClient.java

--- app/src/main/java/com/example/esn_steria_android/RetrofitClient.java
+++ app/src/main/java/com/example/esn_steria_android/RetrofitClient.java
@@ -10,7 +10,7 @@ public class RetrofitClient {
     private static Retrofit retrofit = null;
 
 //    private static final String BASE_URL = "http://172.20.10.5/API/";
-    private static final String BASE_URL = "http://192.168.1.13/API/";
+    private static final String BASE_URL = "http://192.168.1.13/PPE3/API/";
     public static Retrofit getInstance() {
         if (retrofit == null) {
             Gson gson = new GsonBuilder()
